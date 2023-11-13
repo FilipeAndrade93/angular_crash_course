afc7dbbe5d80ab4154e6d4e1744d3c4211330eb3
Filipe Andrade <filipe.andrade@zarph.com>
2023-11-13 13:38:16 +0000

    Added json server. Json server ok. Tasks fetched from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TasksComponent } from "./components/tasks/tasks.component";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -13,4 +15,5 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 })
 export class AppComponent {
  
+    constructor(private http:HttpClient){}
 }
